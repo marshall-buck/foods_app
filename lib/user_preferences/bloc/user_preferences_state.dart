8caad2d45b1ee565f054ecb907a077b5ec905c49
Marshall Buck <dev@marshallbuck.com>
2025-03-13 15:48:53 -0500

    Enhance documentation in UserPreferencesState by adding detailed comments for properties and methods

--- lib/user_preferences/bloc/user_preferences_state.dart
+++ lib/user_preferences/bloc/user_preferences_state.dart
@@ -1,12 +1,17 @@
 part of 'user_preferences_bloc.dart';
 
+/// {@template user_preferences_state}
+/// A state that holds user preferences such as display mode, quick search IDs, and saved foods.
+/// {@endtemplate}
 class UserPreferencesState extends Equatable {
+  /// {@macro user_preferences_state}
   const UserPreferencesState({
     required this.displayMode,
     required this.quickSearchIds,
     required this.savedFoods,
   });
 
+  /// Creates an empty [UserPreferencesState].
   factory UserPreferencesState.empty() {
     return const UserPreferencesState(
       displayMode: '',
@@ -15,10 +20,16 @@ class UserPreferencesState extends Equatable {
     );
   }
 
+  /// The display mode preference of the user.
   final String displayMode;
+
+  /// A list of quick search IDs saved by the user.
   final List<String> quickSearchIds;
+
+  /// A list of foods saved by the user.
   final List<String> savedFoods;
 
+  /// Creates a copy of the current state with optional new values.
   UserPreferencesState copyWith({
     String? displayMode,
     List<String>? quickSearchIds,
