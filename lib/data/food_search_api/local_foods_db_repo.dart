d4a4908adc9344aa77a5cbc3e8f08f108c3cfef8
Marshall Buck <dev@marshallbuck.com>
2025-03-13 15:45:50 -0500

    Improve documentation in LocalFoodsDBRepo by clarifying method descriptions and parameters

--- lib/data/food_search_api/local_foods_db_repo.dart
+++ lib/data/food_search_api/local_foods_db_repo.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:foods_app/data/data.dart';
-
 import 'package:usda_db_package/usda_db_package.dart';
 
 /// {@template foods_db_repo}
@@ -12,12 +11,14 @@ class LocalFoodsDBRepo implements FoodSearchApiInterface {
   LocalFoodsDBRepo({required UsdaDB localDBApi, required FoodsSearchCache cache})
       : _localDBApi = localDBApi,
         _cache = cache;
-  final UsdaDB _localDBApi;
 
+  final UsdaDB _localDBApi;
   final FoodsSearchCache _cache;
 
   /// Provides a [FoodDAO] object from the database.
   /// If the food ID is not found in the database, the Future returns null.
+  ///
+  /// [id] is the unique identifier of the food to be queried.
   @override
   Future<FoodDAO?> queryFood({required int id}) async {
     if (_cache.contains(id)) {
@@ -35,14 +36,15 @@ class LocalFoodsDBRepo implements FoodSearchApiInterface {
   @override
   void clearCache() => _cache.clear();
 
-  /// Provides a List [FoodDAO] objects from the database.
+  /// Provides a List of [FoodDAO] objects from the database.
   /// If no foods are found in the database, the Future returns an empty list.
+  ///
+  /// [searchTerm] is the term used to search for foods in the database.
   @override
   Future<List<FoodDAO>?> queryFoods({required String searchTerm}) async {
     final foods = await _localDBApi.queryFoods(searchString: searchTerm);
 
     if (foods.isEmpty) {
-      // _cache.clear();
       return null;
     } else {
       return foods.map((food) {
@@ -51,11 +53,10 @@ class LocalFoodsDBRepo implements FoodSearchApiInterface {
         _cache.add(id, convertedFood);
         return convertedFood;
       }).toList();
-      // return foods.map((food) => FoodDAO.fromUsdaDB(food!)).toList();
     }
   }
 
-  /// The  [dispose] method closes the database connection and releases any
+  /// The [dispose] method closes the database connection and releases any
   /// resources associated with the database.
   @override
   Future<void> dispose() async {
