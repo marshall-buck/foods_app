3cab3173908181997b24c2aad6593b84387f3355
Marshall Buck <dev@marshallbuck.com>
2025-03-15 14:01:43 -0500

    Add documentation comment for FoodSearchApiInterface class

--- lib/data/food_search_api/food_search_api_interface.dart
+++ lib/data/food_search_api/food_search_api_interface.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:foods_app/data/food_search_api/models/food_dto.dart';
 
+/// Class would be used for both an internal DB and an external api.
 abstract interface class FoodSearchApiInterface {
   Future<FoodDTO?> queryFood({required int id});
   Future<List<FoodDTO>?> queryFoods({required String searchTerm});
