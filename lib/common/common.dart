3ab5d63b8a676048669be09575cbfb0d46e48d80
Marshall Buck <dev@marshallbuck.com>
2025-03-30 10:33:25 -0500

    feat: Remove unused responsive utility and result classes; add documentation for PreferencesNotInitialized exception

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -1,7 +1,7 @@
 export 'exceptions.dart';
 export 'extensions.dart';
 export 'magic.dart';
-export 'responsive_util.dart';
+
 export 'theme/colors.dart';
 export 'theme/text_styles.dart';
 export 'widgets/widgets.dart';
--- lib/common/exceptions.dart
+++ lib/common/exceptions.dart
@@ -1,5 +1,15 @@
+/// {@template preferences_not_initialized}
+/// An exception that is thrown when preferences are accessed before being initialized.
+///
+/// This exception is typically used to indicate that an operation requiring
+/// initialized preferences cannot proceed because the preferences have not
+/// been properly set up.
+/// {@endtemplate}
 class PreferencesNotInitialized implements Exception {
+  /// {@macro preferences_not_initialized}
   PreferencesNotInitialized(this.message);
+
+  /// A message describing the details of the exception.
   final String message;
 
   @override
